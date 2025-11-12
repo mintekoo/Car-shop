be5c6867a06f652d9e2da0323faf651eb1fcf565
Minte <mintesinottamene0917@gmail.com>
2025-11-12 12:27:43 +0300

    chnage domin

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      'localhost'
+      // 'localhost'
+      'api.adinascarrent.com'
     ],
   },
   /* config options here */
